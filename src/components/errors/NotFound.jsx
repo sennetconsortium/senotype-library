5eae8f4cf833bb766a1e9d39135e2ed957ef85ad
maxsibilla <maxsibilla@icloud.com>
2026-04-15 15:57:50 -0400

    Adding 404 page'

---
+++ src/components/errors/NotFound.jsx
@@ -0,0 +1,23 @@
+'use client';
+import { Button, Result } from 'antd';
+import { useRouter } from 'next/navigation';
+import { useContext } from 'react';
+import AppContext from '@/context/AppContext';
+
+export default function NotFound() {
+  const router = useRouter();
+  const { auth } = useContext(AppContext);
+
+  return (
+    <Result
+      status="404"
+      title={404}
+      subTitle="Sorry, the page you visited does not exist."
+      extra={[
+        <Button type={"primary"} key="home" onClick={() => router.push('/search')}>
+          Back Home
+        </Button>,
+      ]}
+    />
+  );
+}
